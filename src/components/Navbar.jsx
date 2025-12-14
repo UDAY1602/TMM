7d672eca6567d7a93e67e4151c57fb23e3ba92d7
Pranav-Sagar <dyapapranav@gmail.com>
2025-12-14 10:44:05 +0530

    Navbar Edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,37 @@
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/icon.svg"; 
+
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
 
-     
+        {/* Logo + Brand Name */}
         <div className="navbar-logo">
-          <Link to="/">
-            <img src={logo} alt="Tanujasreekesh Mines & Minerals" />
+          <Link to="/" className="logo-wrapper">
+            <img src={logo} alt="Tanujasreekesh logo" />
+            <div className="logo-text">
+              <span className="logo-title">TANUJASREEKESH</span>
+              <span className="logo-sub">MINES &amp; MINERALS</span>
+            </div>
           </Link>
         </div>
 
-     
-        <ul className="navbar-links">
-          <li>
-            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/products">Products</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
+        {/* Links + CTA (WRAPPED TOGETHER) */}
+        <div className="navbar-actions">
+          <ul className="navbar-links">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/products">Products</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
 
-        <div className="navbar-cta">
-          <Link to="/contact" className="cta-btn">
-            Get in Touch
-          </Link>
+          <div className="navbar-cta">
+            <Link to="/contact" className="cta-btn">
+              Get in Touch
+            </Link>
+          </div>
         </div>
 
       </div>
