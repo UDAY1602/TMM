e6b308eff48fb506dd64652b8d524af98bfadfa2
Udayk16 <udaykumargovindu16@gmail.com>
2025-12-15 12:38:30 +0530

    done with section 1

--- src/Pages/Home/Section1.jsx
+++ src/Pages/Home/Section1.jsx
@@ -1,7 +1,29 @@
+import section1bg from "../../assets/section1bg.png";
+
 export default function Section1() {
   return (
-    <div style={{ color: "Black", padding: "120px 32px" }}>
-      Section 1 (Home)
+    <div
+      className="h-screen w-full bg-cover bg-center flex items-center"
+      style={{ backgroundImage: `url(${section1bg})` }}
+    >
+      <div className="pl-40">
+        
+        <h1 className="heading-font text-[#eeedd3] text-5xl pb-2">
+          Reliable Minerals.
+        </h1>
+
+        <h1 className="heading-font text-[#eeedd3] text-5xl pb-1">
+          Global Reach.
+        </h1>
+
+        <br />
+      
+        <p className="para-font text-white leading-relaxed text-s">
+          High-performance quartz materials for engineered <br />
+          stone manufacturers. Precision-processed for purity,<br />
+          whiteness, and optimal particle distribution.
+        </p>
+      </div>
     </div>
   );
 }
