7e63232755db9e861393b5cbfa0641e8148b397b
Teja <tejaswavarma@gmail.com>
2025-12-18 09:39:22 +0530

    Added section4 in about page

--- src/Pages/About/Section4.jsx
+++ src/Pages/About/Section4.jsx
@@ -1,7 +1,41 @@
+import aboutsection4bg from "../../Assets/aboutsection4bg.png";
 export default function Section4() {
   return (
-    <div style={{ color: "Black", padding: "120px 32px" }}>
-      Section 4 (About)
-    </div>
+    <section
+      className="
+        relative w-full
+        h-[320px]
+        sm:h-[420px]
+        lg:h-[565px]
+        flex justify-center
+        pt-16 sm:pt-20 lg:pt-28
+        px-6 sm:px-10
+  "
+      style={{
+        backgroundImage: `url(${aboutsection4bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/* Content */}
+      <div className="relative z-10 max-w-4xl text-center text-[#eeedd3]">
+        {/* Label */}
+        <p className="bankgothiclightreg text-xs sm:text-xl tracking-widest uppercase text-[#eeedd3] mb-2">
+          WHAT MAKES
+        </p>
+
+        {/* Main Heading */}
+        <h1 className="rebeltypedog text-1xl sm:text-2xl md:text-3xl mb-3">
+          TANUJASREEKESH
+        </h1>
+
+        {/* Body Text */}
+        <p className="montserratthin text-sm md:text-2xl text-[#eeedd3] leading-relaxed">
+          Our leadership combines industry experience, technical expertise, and <br/>
+          a vision to deliver high-value mineral solutions for global markets.
+        </p>
+      </div>
+    </section>
   );
 }
