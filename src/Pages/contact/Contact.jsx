3f01dd2ccd6eec9c46afaf77b7384bc758891f2a
Udayk16 <udaykumargovindu16@gmail.com>
2025-12-30 14:54:46 +0530

    made contact page  without fonts

---
+++ src/Pages/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import ContactHero from "./ContactHero";
+import ContactForm from "./ContactForm";
+import ContactInfo from "./ContactInfo";
+
+export default function Contact() {
+  return (
+    <main>
+      <ContactHero />
+      <ContactForm />
+      <ContactInfo />
+    </main>
+  );
+}
